291cac59ed3cee6da021c38b58101b3c0547aa3c
Kaz Namba <kaz.namba@gmail.com>
2018-07-05 16:23:05 +0900

    refactor: Remove /users

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require('morgan')
 const stylus = require('stylus')
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
 
 const app = express()
 
@@ -24,7 +23,6 @@ app.use(stylus.middleware(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
