c8f4821030ebcfab0c55297fc0e27a01febefcdc
Kaz Namba <kaz.namba@gmail.com>
2018-07-05 16:21:09 +0900

    chore: Ingore generated stylesheet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const stylus = require('stylus')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -19,6 +20,7 @@ app.use(express.urlencoded({
   extended: false
 }))
 app.use(cookieParser())
+app.use(stylus.middleware(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
